38d6a9d933aaeca22b93e30d2381ea8551cca0b0
alexandrina2798 <alexandrina2798@mail.ru>
2021-05-06 22:35:51 +0300

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+import utils.BeforeUtils;
+
+public class Main {
+    public static void main(String[] args) {
+        // write your code here
+        BeforeUtils.createData();
+    }
+}
---
+++ src/test/java/DbClient.java
@@ -0,0 +1,130 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import utils.BeforeUtils;
+
+import javax.xml.transform.Result;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class DbClient {
+
+    @Test
+    public void countTest() throws SQLException, ClassNotFoundException {
+        Connection connection = new BeforeUtils().getConnection();
+        BeforeUtils.createData();
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery("select * from public.animal");
+
+        int expectedCount = 10;
+        int realCount = 0;
+        while (resultSet.next()) {
+            realCount = resultSet.getRow();
+        }
+        Assertions.assertTrue(realCount == expectedCount);
+        connection.close();
+    }
+
+    @Test
+    public void insertTest() throws SQLException, ClassNotFoundException {
+        Connection connection = new BeforeUtils().getConnection();
+        BeforeUtils.createData();
+        Statement statement = connection.createStatement();
+        int row = 0;
+        int expectedInsertedRows = 0;
+        int realInsertedRows = 0;
+        while (row < 10) {
+            row++;
+            try {
+                realInsertedRows += statement.
+                        executeUpdate("INSERT INTO public.animal (id, \"name\", age, \"type\", sex, place) " +
+                                "VALUES(" + row + " , 'Лишний" + row + " ', 2, 1, 1, 1);\n");
+
+            } catch (Exception JdbcSQLIntegrityConstraintViolationException) {
+            }
+        }
+
+        Assertions.assertTrue(expectedInsertedRows == realInsertedRows);
+        connection.close();
+    }
+
+    @Test
+    public void insertNullTest() throws SQLException, ClassNotFoundException {
+        Connection connection = new BeforeUtils().getConnection();
+        BeforeUtils.createData();
+        Statement statement = connection.createStatement();
+        int row = 0;
+        int expectedInsertedRows = 0;
+        int realInsertedRows = 0;
+        try {
+            realInsertedRows += statement.
+                    executeUpdate("INSERT INTO public.workman (id, \"name\", age, \"position\") " +
+                            "VALUES(10214, null, 23, 1);\n");
+        } catch (Exception JdbcSQLIntegrityConstraintViolationException) {
+        }
+
+        Assertions.assertTrue(expectedInsertedRows == realInsertedRows);
+        connection.close();
+    }
+
+    @Test
+    public void oneMorePlaceTest() throws SQLException, ClassNotFoundException {
+        Connection connection = new BeforeUtils().getConnection();
+        BeforeUtils.createData();
+        Statement statement = connection.createStatement();
+        int insertedRows = 0;
+        try {
+            insertedRows = statement.executeUpdate("INSERT INTO public.places (id, \"row\", place_num, \"name\") " +
+                    "VALUES(112, 112, 18512, 'Тестовый загон');\n");
+        } catch (Exception JdbcSQLIntegrityConstraintViolationException) {
+        }
+
+        ResultSet resultSet = statement.executeQuery("select * from public.places");
+
+        int expectedCount = 6;
+        int realCount = 0;
+        while (resultSet.next()) {
+            realCount = resultSet.getRow();
+        }
+
+        Assertions.assertEquals(1, insertedRows);
+        Assertions.assertEquals(realCount, expectedCount);
+
+        try {
+            statement.executeUpdate("delete from public.places where id = 112");
+        } catch (Exception JdbcSQLIntegrityConstraintViolationException) {
+        }
+
+
+        connection.close();
+    }
+
+    @Test
+    public void zooTest() throws SQLException, ClassNotFoundException {
+        Connection connection = new BeforeUtils().getConnection();
+        BeforeUtils.createData();
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery("select * from public.zoo");
+
+        ArrayList<String> expectedZooList = new ArrayList<String>();
+        ArrayList<String> actualZooList = new ArrayList<String>();
+        expectedZooList.add("Центральный");
+        expectedZooList.add("Северный");
+        expectedZooList.add("Западный");
+
+        int expectedCount = 3;
+        int realCount = 0;
+        while (resultSet.next()) {
+            realCount = resultSet.getRow();
+            actualZooList.add(resultSet.getString("name"));
+        }
+        Assertions.assertEquals(expectedCount, realCount);
+        Assertions.assertEquals(expectedZooList, actualZooList);
+
+
+        connection.close();
+    }
+
+
+}
